af4adddb96df749c11da79ec16fd52a7def88120
gibsongondwe <131948682+gibsongondwe@users.noreply.github.com>
2024-05-20 08:29:43 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        fluid:"repeat(auto-fit,minmax(15rem,1fr))",
+      },
+    },
+  },
+  plugins: [ require('tailwindcss'),
+  require('autoprefixer'),],
+}
